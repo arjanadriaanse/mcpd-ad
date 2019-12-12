d855215b1f9a73ba85c61ee3970deb32739b268c
Arjan Adriaanse <arjan@adriaan.se>
2019-12-12 16:25:05 +0100

    clean up forward algorithm

--- src/Forward.hs
+++ src/Forward.hs
@@ -4,44 +4,21 @@ import Language
 import Algebra
 
 differentiate :: Term -> Term
-differentiate = foldTerm (fVar, fReal, fInt, fPair, fCase, fNew, fLook, fUp, fLen, fMap, 
-        fFold, fFun, fApp, fNeg, fSig, fAdd, fMult, fDot,
-        (fTInt, fTReal, fTPair, fTArray, fTFun)) where
-            fVar  = Var
-            fReal n = Pair (CReal n) (CReal 0)
-            fInt  = CInt
-
-            -- Pair, case
-            fPair = Pair
-            fCase = Case
-            
-            -- The rest
-            fNew = New
-            fLook = Lookup
-            fUp = Update
-            fLen = Length
-            fMap = Map
-            fFold = Fold
-            fFun = Fun
-            fApp = FunApp
-            
-            -- Operators
-            fNeg = Neg
-            fSig t = Case t "x" "x'"
-              (Fun TReal (TPair TReal TReal) "y"
-               (Pair (Var "y") (Var "x'" `Mult` Var "y" `Mult`
-                                (CReal 1 `Add` Neg (Var "y"))))
-               `FunApp` Sigmoid (Var "x"))
-            fAdd t s = Case t "x" "x'" (Case s "y" "y'"
-                                        (Pair (Var "x" `Add` Var "y")
-                                         (Var "x'" `Add` Var "y'")))
-            fMult t s = Case t "x" "x'" (Case s "y" "y'"
-                                         (Pair (Var "x" `Add` Var "y")
-                                          ((Var "x" `Mult` Var "y'") `Add`
-                                           (Var "x'" `Mult` Var "y"))))
-            fDot     = Dot
-            fTInt = TInt
-            fTReal = TPair TReal TReal
-            fTPair = TPair
-            fTArray = TArray
-            fTFun = TFun
+differentiate = foldTerm
+  (Var, fReal, CInt, Pair, Case, New, Lookup, Update, Length,
+   Map, Fold, Fun, FunApp, Neg, fSigmoid, fAdd, fMult, Dot,
+   (TInt, fTReal, TPair, TArray, TFun)) where
+  fTReal = TPair TReal TReal
+  fReal n = Pair (CReal n) (CReal 0)
+  fAdd t s = Case t "x" "x'" (Case s "y" "y'"
+                              (Pair (Var "x" `Add` Var "y")
+                               (Var "x'" `Add` Var "y'")))
+  fMult t s = Case t "x" "x'" (Case s "y" "y'"
+                               (Pair (Var "x" `Add` Var "y")
+                                ((Var "x" `Mult` Var "y'") `Add`
+                                 (Var "x'" `Mult` Var "y"))))
+  fSigmoid t = Case t "x" "x'"
+               (Fun TReal (TPair TReal TReal) "y"
+                (Pair (Var "y") (Var "x'" `Mult` Var "y" `Mult`
+                                 (CReal 1 `Add` Neg (Var "y"))))
+                `FunApp` Sigmoid (Var "x"))
