05197feb0be90e4be87d5f8887ebe729a4834508
Arjan Adriaanse <arjan@adriaan.se>
2019-12-13 10:29:25 +0100

    fix error in muliplication case of forward algorithm

--- src/Forward.hs
+++ src/Forward.hs
@@ -14,7 +14,7 @@ differentiate = foldTerm
                               (Pair (Var "x" `Add` Var "y")
                                (Var "x'" `Add` Var "y'")))
   fMult t s = Case t "x" "x'" (Case s "y" "y'"
-                               (Pair (Var "x" `Add` Var "y")
+                               (Pair (Var "x" `Mult` Var "y")
                                 ((Var "x" `Mult` Var "y'") `Add`
                                  (Var "x'" `Mult` Var "y"))))
   fSigmoid t = Case t "x" "x'"
