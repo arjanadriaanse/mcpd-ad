b967dca76ee54fa74755a2857c8a8095fd7e9ea9
Arjan Adriaanse <arjan@adriaan.se>
2020-01-01 13:53:40 +0100

    improve readability

--- src/Variable.hs
+++ src/Variable.hs
@@ -5,7 +5,6 @@ module Variable where
 
 import Language
 import Algebra
-import PrettyPrint
 
 import qualified Data.Map.Strict as M
 import Control.Monad
@@ -22,7 +21,7 @@ instance Counter AlphaRenameState where
 
 -- | Generates a fresh variable name
 fresh :: Counter a => Identifier -> State a Identifier
-fresh x = add >>= return . (x ++) . show
+fresh x = (x ++) . show <$> add
 
 declare :: Identifier -> State AlphaRenameState Identifier
 declare x = do
@@ -41,7 +40,7 @@ type AlphaRenameState = (Int, M.Map Identifier Identifier)
 
 -- | Make all variable names unique
 alphaRename :: Term -> Term
-alphaRename t = evalState (foldTerm (fVar, return . CReal, return . CInt, (\x y -> (liftM2 CArray) x (sequence y)), liftM2 Pair, fFun, liftM Sigmoid, liftM2 Add, liftM2 Mult, liftM2 Dot, liftM2 IntAdd, liftM2 IntMult, liftM2 New, liftM Length, liftM2 Lookup, liftM3 Update, liftM2 Map,  liftM3 Fold, fCase, liftM2 Apply,
+alphaRename t = evalState (foldTerm (fVar, return . CReal, return . CInt, \x y -> (liftM2 CArray) x (sequence y), liftM2 Pair, fFun, liftM Sigmoid, liftM2 Add, liftM2 Mult, liftM2 Dot, liftM2 IntAdd, liftM2 IntMult, liftM2 New, liftM Length, liftM2 Lookup, liftM3 Update, liftM2 Map,  liftM3 Fold, fCase, liftM2 Apply,
         (return TReal, return TInt, liftM TArray, liftM2 TPair, liftM2 TFun)) t) (0, M.empty) where
   fVar x = Var <$> gets ((M.! x) . snd)
   fFun t1 t2 x e = local $ Fun <$> t1 <*> t2 <*> declare x <*> e
