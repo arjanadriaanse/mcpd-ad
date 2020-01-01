1bccebd95face0fd7f0e9c50cb732419db0a2db6
Arjan Adriaanse <arjan@adriaan.se>
2020-01-01 13:56:32 +0100

    backward work in progress

--- src/Backward.hs
+++ src/Backward.hs
@@ -1,21 +1,20 @@
-module Forward where 
+module Backward where 
 
 import Language
 import Algebra
-import Variable
 
-differentiate :: Term -> Term
-differentiate = foldTerm
-  (Var, fCReal, CInt, CArray, Pair, Fun, fSigmoid, fAdd, fMult, Dot,
-   IntAdd, IntMult, New, Length, Lookup, Update, Map, Fold, Case, Apply,
-   (fTReal, TInt, TArray, TPair, TFun)) . alphaRename where
-  fTReal = TPair TReal TReal
-  fCReal n = Pair (CReal n) (CReal 0)
+differentiate :: Int -> Term -> Term
+differentiate k = foldTerm
+  (Var, fReal, CInt, Pair, Case, New, Lookup, Update, Length,
+   Map, Fold, Fun, FunApp, fSigmoid, fAdd, fMult, Dot,
+   (TInt, fTReal, TPair, TArray, TFun)) where
+  fTReal = TPair TReal (TFun TReal (TArray TReal))
+  fReal n = Pair (CReal n) (Fun TReal (TArray TReal) "z" (New TReal (CInt k)))
   fAdd t s = Case t "x" "x'" (Case s "y" "y'"
                               (Pair (Var "x" `Add` Var "y")
                                (Var "x'" `Add` Var "y'")))
   fMult t s = Case t "x" "x'" (Case s "y" "y'"
-                               (Pair (Var "x" `Mult` Var "y")
+                               (Pair (Var "x" `Add` Var "y")
                                 ((Var "x" `Mult` Var "y'") `Add`
                                  (Var "x'" `Mult` Var "y"))))
   fSigmoid t = Case t "x" "x'"
@@ -23,4 +22,4 @@ differentiate = foldTerm
                 (Pair (Var "y")
                  (Var "x'" `Mult` Var "y" `Mult`
                   (CReal 1 `Add` (CReal (-1) `Mult` Var "y"))))
-                `Apply` Sigmoid (Var "x"))
+                `FunApp` Sigmoid (Var "x"))
