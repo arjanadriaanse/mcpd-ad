c18da9a7f510b93adba57c6df199e4dd64a6f273
annaproxy <annaproxy@protonmail.com>
2020-01-07 13:16:08 +0100

    sugar examples in main

--- src/Main.hs
+++ src/Main.hs
@@ -9,14 +9,10 @@ import AbstractMachine
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
 
-example0 :: Term 
-example0 = 2 * var "x"
-
 example :: Term
-example = test $$ (10 $* 1)
-
-test :: Term
-test = differentiate $ fun [("y", real)] (var "y" * Var "y" + Var "y", real)
+example = df $$ (10 $* 1)
+    where
+        df = differentiate $ fun [("y", real)] (var "y" * Var "y" + Var "y", real)
 
 exampleNest :: Term 
 exampleNest = fun [("y", real)] (f $$ 20, real) $$ 8000
@@ -24,17 +20,13 @@ exampleNest = fun [("y", real)] (f $$ 20, real) $$ 8000
         f = fun [("y", real)] (var "x" + var "y", real) 
 
 exampleFst :: Term
-exampleFst = Apply (Fun (TPair TReal TReal) (TReal) "x" (Case (Var "x") "id1" "id2" (Var "id1") )) (Pair (CReal 1) (CReal 20))
-
-example2 :: Term
-example2 = Fun TReal TReal "x" ((Fun TReal TReal "x" (Var "x" `Mult` Var "x")) `Apply` (Var "x"))
+exampleFst = fun [("x", TPair real real)] ( case_ (var "x") "id1" "id2" (var "id1") , real) $$ (Pair 10 20) 
 
 exampleMap :: Term 
-exampleMap = Map (Fun TReal TReal "x" ( Add (Var "x") (CReal 20))) (New TReal (CInt 10))
+exampleMap = map_ (fun [("x", real)] ((Var "x") + 20, real)) (new real (CInt 10))
 
 exampleDot :: Term 
-exampleDot = Dot exampleMap exampleMap
-
+exampleDot = dot exampleMap exampleMap
 
 newIndexList :: Term 
 newIndexList = update (new real (CInt 10)) (CInt 1) (5.1 + 9)
@@ -43,7 +35,7 @@ examplef :: Term
 examplef = fold f 0 exampleMap where 
     f = fun [("b", real), ("x", real)] (var "x" + var "b", real) 
 
-
-testNested = f $$ CReal 10 $$ 5 $$ 3
+exampleNested :: Term
+exampleNested = f $$ 10 $$ 5 $$ 3
     where 
-        f = fun [("x", TReal),("y", TReal), ("z", TReal)] ((Var "x") * ((Var "y") + (Var "z")), TReal) 
+        f = fun [("x", real),("y", real), ("z", real)] ((var "x") * ((var "y") + (var "z")), real) 
