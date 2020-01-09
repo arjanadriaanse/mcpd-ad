741cef62535ebec5f5d4174adff7dbc3ff30d623
Arjan Adriaanse <arjan@adriaan.se>
2020-01-09 11:16:13 +0100

    add differentiation cases for typed binary operators

--- src/Forward.hs
+++ src/Forward.hs
@@ -12,14 +12,14 @@ differentiate = foldTerm
   fTReal = real $* real
   fCReal n = CReal n $* 0
   fBinOp Add (Just TReal) t s = case_ t "x" "x'" $ case_ s "y" "y'" $
-             (var "x" + var "y") $*
-             (var "x'" + var "y'")
+    (var "x" + var "y") $* (var "x'" + var "y'")
   fBinOp Mult (Just TReal) t s = case_ t "x" "x'" $ case_ s "y" "y'" $
-              (var "x" * var "y") $*
-              (var "x" * var "y'" + var "x'" * var "y")
-  fSigmoid t = case_ t "x" "x'" $
-               let_ "y" (sigmoid (var "x"), real)
-               (var "y" $*
-                (var "x'" * var "y" * (1 - var "y")),
-                 real $* real)
-
+    (var "x" * var "y") $* (var "x" * var "y'" + var "x'" * var "y")
+  fBinOp Add (Just TInt) t s = BinOp Add (Just TInt) t s
+  fBinOp Mult (Just TInt) t s = BinOp Add (Just TInt) t s
+  fBinOp Add (Just (TArray a)) t s = undefined -- TODO
+  fBinOp Mult (Just (TArray a)) t s = undefined -- TODO
+  fBinOp Dot (Just (TArray a)) t s = undefined -- TODO
+  fBinOp _ _ _ _ = error "operation not supported"
+  fSigmoid t = case_ t "x" "x'" $ let_ "y" (sigmoid (var "x"), real)
+    (var "y" $* (var "x'" * var "y" * (1 - var "y")), real $* real)
