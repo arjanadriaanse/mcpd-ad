865ad286cfd5f16b52340dd26a44fb83d1c8814f
A.H. Kole <a.h.kole@uu.nl>
2020-01-14 17:11:13 +0100

    Added sugar for Fst and Snd functions

--- src/Language.hs
+++ src/Language.hs
@@ -109,5 +109,8 @@ case_ = Case
 ($$) :: Term -> Term -> Term
 ($$) = Apply
 
+fst_ :: Type -> Type -> Term
+fst_ tau1 tau2 = fun [("x", tau1 $* tau2)] (case_ (var "x") "y" "z" $ var "y", tau1) 
 
-
+snd_ :: Type -> Type -> Term
+snd_ tau1 tau2 = fun [("x", tau1 $* tau2)] (case_ (var "x") "y" "z" $ var "z", tau2)
